e5cb9f9019fc2ca9e6c18f15a46b8177f9c85369
Roy Sonn <roysonnpl@gmail.com>
2025-10-28 15:35:31 +0200

    Increment the ball’s width & height by 50px (ball click) Display the updated diameter inside the ball

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 'use strict'
 
 function onBallBlick() {
-    console.log('This is a ball')
+    var ball = document.querySelector('.ball')
+    var currSize = parseInt(ball.style.width)
+
+    if (isNaN(currSize)) currSize = 100
+
+    currSize += 50
+    ball.style.width = currSize + 'px'
+    ball.style.height = currSize + 'px'
+
+    ball.textContent = currSize
 }
